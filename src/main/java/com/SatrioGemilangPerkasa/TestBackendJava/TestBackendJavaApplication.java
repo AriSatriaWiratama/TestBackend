4e40ce8f335ad1fc25cc0f97bb8ae9863acfaadb
Ari Satria Wiratama <arisatriawiratama1@gmail.com>
2024-01-06 14:07:28 +0700

    Start Project

---
+++ src/main/java/com/SatrioGemilangPerkasa/TestBackendJava/TestBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.SatrioGemilangPerkasa.TestBackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SatrioGemilangPerkasa/TestBackendJava/TestBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SatrioGemilangPerkasa.TestBackendJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
